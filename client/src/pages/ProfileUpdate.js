1f3a90153cfbff1def1e0fa56fbd57d23e646e95
Mostafa Maki <mostafa_m9@yahoo.com>
2022-12-17 10:18:56 -0800

    implemented adding interest feature

--- client/src/pages/ProfileUpdate.js
+++ client/src/pages/ProfileUpdate.js
@@ -1,12 +1,9 @@
 import React, { useState } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { updateUser } from '../components/UserProfile';
 import { useQuery, useMutation } from '@apollo/client';
-// import { USER_UPDATE_REQUEST } from '../utils/actions';
 import { QUERY_SINGLE_PROFILE } from '../utils/queries'
-import { UPDATE_USER, DELETE_PROFILE, UPDATE_BIO } from '../utils/mutations';
+import { ADD_INTEREST, DELETE_PROFILE, UPDATE_BIO } from '../utils/mutations';
 import Auth from '../utils/auth';
-// imported from chakra UI
+// imported elements from chakra UI
 import {
   Box,
   Avatar,
@@ -32,28 +29,24 @@ import {
   ModalFooter,
   Editable,
   EditableInput,
-  EditablePreview
+  EditablePreview,
+  Textarea
 } from '@chakra-ui/react';
 
+// function to deal with handling adding interests, updating user bio, or deleting a user's profile
 const UpdateProfile = (props) => {
-
+  // get the profile id from the stored token
   const userId = Auth.getProfile();
   const { loading, data } = useQuery(QUERY_SINGLE_PROFILE, {
     variables: { profileId: userId.data._id }
   });
-
+  // get the profile data from the token
   const profile = data?.profile || {};
   const [removeProfile, { error }] = useMutation(DELETE_PROFILE);
-
+  // function to handle deleting the user's profile
   const handleDeleteProfile = async () => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
     const testId = Auth.getProfile();
-    //token
-    console.log(token);
-    //object
-    console.log(testId)
-    //id
-    console.log(testId.data._id)
     if (!token) {
       return false;
     }
@@ -69,9 +62,26 @@ const UpdateProfile = (props) => {
     }
     Auth.logout();
   };
+  const [interest, setInterest] = useState('');
+  const [addInterest, { er }] = useMutation(ADD_INTEREST);
+  // function to handle adding an interest
+  const handleAddingInterest = async (event) => {
 
-  const [ userBioText, setUserBioText ] = useState('');
-  const [updateBio, { err } ] = useMutation(UPDATE_BIO);
+    try {
+      const { data } = await addInterest({
+        variables: {
+          profileId: userId.data._id,
+          interest
+        }
+      });
+      setInterest('');
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  const [userBioText, setUserBioText] = useState('');
+  const [updateBio, { err }] = useMutation(UPDATE_BIO);
   const handleBioUpdate = async () => {
     try {
       const { data } = await updateBio({
@@ -85,172 +95,64 @@ const UpdateProfile = (props) => {
     }
   }
 
-  // //Get the user from localstorage and pass to the initial states
-  // const userLogin = useSelector(state => state.userLogin);
-  // const { userInfo } = userLogin;
-  // console.log(userInfo);
-
-  // //dispatch
-  // const dispatch = useDispatch();
-  //submit
-  // const handleFormSubmit = async (event) => {
-  //   event.preventDefault();
-
-  //   const mutationResponse = await updateUser({
-  //     variables: {
-  //       email: formState.email,
-  //       password: formState.password,
-  //       firstName: formState.firstName,
-  //       lastName: formState.lastName,
-  //     },
-  //   });
-  //   const token = mutationResponse.data.addUser.token;
-  //   Auth.login(token);
-  // };
-
-  // const handleChange = (event) => {
-  //   const { name, value } = event.target;
-  //   setFormState({
-  //     ...formState,
-  //     [name]: value,
-  //   });
-  // };
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <>
-      <Text>What would you like to do {profile.firstName}?</Text>
-      <Text>Add more interests</Text>
-      <Text>Update Bio</Text>
-<Editable defaultValue='Take some chakra'>
-  <EditablePreview />
-  <EditableInput />
-</Editable>
-      <div>
-        <Center>
-          <Box bg='orange.400' w='100%' p={4} borderRadius='full'>
-            Update Your Profile
-          </Box>
-        </Center>
-        <Stack direction='row' h='5px' p={4}>
-          <Divider />
-        </Stack>
-        <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
-          <Flex p={8} flex={1} align={'center'} justify={'center'}>
-            <Stack spacing={6} w={'full'} maxW={'lg'}>
-              <Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}>
-                <Text
-                  as={'span'}
-                  position={'relative'}
-                  _after={{
-                    content: "''",
-                    width: 'full',
-                    height: useBreakpointValue({ base: '20%', md: '30%' }),
-                    position: 'absolute',
-                    bottom: 1,
-                    left: 0,
-                    bg: 'orange.400',
-                    zIndex: -1,
-                  }}>
-                  {/* {user.name} */}
-                  User Name
-                </Text>
-              </Heading>
-              {/* <Form > */}
-              {/* <FormControl onSubmit={handleFormSubmit}> */}
-              <FormControl>
-                <FormLabel>First Name:</FormLabel>
-                <Input
-
-                  // onChange={handleChange}
-                  className="form-input"
-                  placeholder="New First Name"
-                  name="firstName"
-                  type="firstName"
-                />
-              </FormControl>
-              <FormControl>
-                <FormLabel>Last Name:</FormLabel>
-                <Input
-
-                  // onChange={handleChange}
-                  className="form-input"
-                  placeholder="New Last Name"
-                  name="lastName"
-                  type="lastName"
-                />
-              </FormControl>
-              <FormControl>
-                <FormLabel>Email:</FormLabel>
-                <Input
-                  // onChange={handleChange}
-                  className="form-input"
-                  placeholder="New Email"
-                  name="email"
-                  type="email"
-                />
-              </FormControl>
-              <FormControl>
-                <FormLabel>Password:</FormLabel>
-                <Input
-                  // onChange={handleChange}
-                  className="form-input"
-                  placeholder="New password"
-                  name="password"
-                  type="password"
-                />
-              </FormControl>
-              <FormControl>
-                <FormLabel>Avatar:</FormLabel>
-                <Avatar
-                  size={'xl'}
-                  // src={profile.image}
-                  // onChange={handleChange}
-                  alt={'Avatar Alt'}
-                  mb={4}
-                  pos={'relative'}
-                  _after={{
-                    content: '""',
-                    w: 4,
-                    h: 4,
-                    bg: 'green.300',
-                    border: '2px solid white',
-                    rounded: 'full',
-                    pos: 'absolute',
-                    bottom: 0,
-                    right: 3,
-                  }}
-                />
-              </FormControl>
-              <Link to='/Profile'><Button rounded={'full'} type='submit'>
-                Save Profile
-              </Button></Link>
-              {/* Delete button modal */}
-              <Button bg={"red.400"} size='lg' onClick={onOpen}>
-                DELETE PROFILE
-              </Button>
-              <Modal isOpen={isOpen} onClose={onClose}>
-                <ModalOverlay />
-                <ModalContent>
-                  <ModalHeader>Delete Profile?</ModalHeader>
-                  <ModalCloseButton />
-                  <ModalBody>
-                    <Text>Are you sure you want to delete your profile? All of your data will be permanently removed.</Text>
-                  </ModalBody>
-                  <ModalFooter>
-                    <Button bg={"orange.300"} mr={3} onClick={onClose}>
-                      Cancel
-                    </Button>
-                    <Button variant='ghost' bg={"red.400"} onClick={() => handleDeleteProfile()}>Delete profile</Button>
-                  </ModalFooter>
-                </ModalContent>
-              </Modal>
-              {/* </Form> */}
-            </Stack>
-          </Flex>
-
-        </Stack>
-      </div>
+      <Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}>
+        <Text
+          as={'span'}
+          position={'relative'}
+          _after={{
+            content: "''",
+            width: 'full',
+            height: useBreakpointValue({ base: '20%', md: '30%' }),
+            position: 'absolute',
+            bottom: 1,
+            left: 0,
+            zIndex: -1,
+          }}>
+          What would you like to do, {profile.firstName}?
+        </Text>
+      </Heading>
+      <FormControl>
+        <FormLabel>Add more interests?</FormLabel>
+        <Input
+          placeholder="i.e. Hiking"
+          name="interest"
+          type="interest"
+          value={interest}
+          onChange={(e) => setInterest(e.target.value)}
+        />
+        <Button onClick={() => handleAddingInterest()}>Add</Button>
+      </FormControl>
+      <FormControl>
+        <FormLabel>
+          Update your bio?
+        </FormLabel>
+        <Textarea >{profile.userBio}</Textarea>
+        <Button>Update</Button>
+      </FormControl>
+      {/* Delete button modal */}
+      <Button bg={"red.400"} size='lg' onClick={onOpen}>
+        DELETE PROFILE
+      </Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Delete Profile?</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text>Are you sure you want to delete your profile? All of your data will be permanently removed.</Text>
+          </ModalBody>
+          <ModalFooter>
+            <Button bg={"orange.300"} mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button variant='ghost' bg={"red.400"} onClick={() => handleDeleteProfile()}>Delete profile</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </>
 
   );
